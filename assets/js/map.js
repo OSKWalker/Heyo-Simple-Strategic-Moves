ec22e743d365b849d7476d098ceda6b123ade40d
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-08-06 11:08:47 -0400

    saving progress on icons

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,26 +1,29 @@
 let mp_map;
 let mp_centerLng = -83.951859;
 let mp_centerLat = 33.972199;
-
 // Initialize and add the map
 function initMap() {
   mp_map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: new google.maps.LatLng(mp_centerLat, mp_centerLng),
   });
-
 }
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
 const eqfeed_callback = function (mp_results) {
+  const mp_image = {
+    url: "./assets/images/img.png",
+    size: new google.maps.Size(20, 32),
+    origin: new google.map.Point(0, 0),
+    anchor: new google.maps.Point(0, 32),
+  };
   mp_results.forEach((element) => {
-   // console.log(element);
     const latLng = new google.maps.LatLng(element.latitude, element.longitude);
     new google.maps.Marker({
       position: latLng,
       map: mp_map,
+      icon: mp_image,
     });
   });
 };
-
